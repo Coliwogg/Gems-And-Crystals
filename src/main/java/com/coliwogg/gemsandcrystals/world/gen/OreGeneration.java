a010b497660bff2bda09bf1faa77d8ffef2d5017
Colin <coliwogg@outlook.com>
2021-10-24 12:46:59 -0300

    Updated mod to version 1.17.1-1.3.4

--- src/main/java/com/coliwogg/gemsandcrystals/world/gen/OreGeneration.java
+++ src/main/java/com/coliwogg/gemsandcrystals/world/gen/OreGeneration.java
@@ -1,16 +1,20 @@
 package com.coliwogg.gemsandcrystals.world.gen;
 
 import com.coliwogg.gemsandcrystals.GemsandCrystals;
+import com.google.common.collect.ImmutableList;
 import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeGenerationSettings;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
-import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
+import net.minecraft.world.level.levelgen.feature.configurations.ReplaceBlockConfiguration;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,35 +24,24 @@ public class OreGeneration {
 
     @SubscribeEvent
     public static void generateOres(final BiomeLoadingEvent event) {
-
         for (OreType ore : OreType.values()) {
             if (!(event.getCategory().equals(Biome.BiomeCategory.NETHER) || event.getCategory().equals(Biome.BiomeCategory.THEEND))) {
-
-                OreConfiguration oreFeatureConfig = new OreConfiguration(
-                        OreConfiguration.Predicates.NATURAL_STONE,
-                        ore.getBlock().get().defaultBlockState(), ore.getMaxVeinSize());
-
-                generateOre(ore.getBlock().get(), oreFeatureConfig, event.getGeneration(), ore.getMinHeight(), ore.getMaxHeight(), ore.getMaxVeinSize());
+                if (ore.getGenerateOreToggle()) {
+                    ImmutableList<OreConfiguration.TargetBlockState> ORE_TARGET_LIST = ImmutableList.of(OreConfiguration.target(OreConfiguration.Predicates.STONE_ORE_REPLACEABLES, ore.getBlock().get().defaultBlockState()), OreConfiguration.target(OreConfiguration.Predicates.DEEPSLATE_ORE_REPLACEABLES, ore.getDeepslateBlock().get().defaultBlockState()));
+                    ConfiguredFeature<?, ?> ORE_TYPE = register(ore.getBlock().get(), Feature.REPLACE_SINGLE_BLOCK.configured(new ReplaceBlockConfiguration(ORE_TARGET_LIST)).rangeUniform(VerticalAnchor.absolute(ore.getMinHeight()), VerticalAnchor.absolute(ore.getMaxHeight())).squared().count(UniformInt.of(ore.getMinVeinSize(), ore.getMaxVeinSize())));
+                    generateOre(event.getGeneration(), ORE_TYPE);
+                }
             }
         }
 
-        for (DeepslateOreType ore : DeepslateOreType.values()) {
-            if (!(event.getCategory().equals(Biome.BiomeCategory.NETHER) || event.getCategory().equals(Biome.BiomeCategory.THEEND))) {
-                OreConfiguration oreFeatureConfig = new OreConfiguration(
-                        OreConfiguration.Predicates.DEEPSLATE_ORE_REPLACEABLES,
-                        ore.getBlock().get().defaultBlockState(), ore.getMaxVeinSize());
-
-                generateOre(ore.getBlock().get(), oreFeatureConfig, event.getGeneration(), ore.getMinHeight(), ore.getMaxHeight(), ore.getMaxVeinSize());
-            }
-        }
     }
 
-    private static void generateOre(Block ore, OreConfiguration oreFeatureConfig, BiomeGenerationSettingsBuilder settings, int minHeight, int maxHeight, int maxVeinSize) {
-        settings.addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, Feature.ORE.configured(oreFeatureConfig).rangeUniform(VerticalAnchor.absolute(minHeight), VerticalAnchor.absolute(maxHeight)).squared().count(maxVeinSize));
-        registerOreFeature(ore, oreFeatureConfig, maxVeinSize);
+    private static <FC extends FeatureConfiguration> ConfiguredFeature<?, ?> register(Block ore, ConfiguredFeature<FC, ?> pConfiguredFeature) {
+        return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, ore.getRegistryName(), pConfiguredFeature);
     }
 
-    private static ConfiguredFeature<?, ?> registerOreFeature(Block ore, OreConfiguration oreFeatureConfig, int maxVeinSize) {
-        return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, ore.getRegistryName(), Feature.ORE.configured(oreFeatureConfig).squared().count(maxVeinSize));
+    public static void generateOre(BiomeGenerationSettings.Builder pBuilder, ConfiguredFeature<?, ?> ore) {
+        pBuilder.addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, ore);
     }
+
 }
--- src/main/java/com/coliwogg/gemsandcrystals/world/gen/OreType.java
+++ src/main/java/com/coliwogg/gemsandcrystals/world/gen/OreType.java
@@ -6,25 +6,40 @@ import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.util.Lazy;
 
 public enum OreType {
-    RUBY(Lazy.of(BlockInit.RUBY_ORE), OreGenConfig.ruby_size.get(), OreGenConfig.ruby_min_height.get(), OreGenConfig.ruby_max_height.get()),
-    SAPPHIRE(Lazy.of(BlockInit.SAPPHIRE_ORE), OreGenConfig.sapphire_size.get(), OreGenConfig.sapphire_min_height.get(), OreGenConfig.sapphire_max_height.get()),
-    TOPAZ(Lazy.of(BlockInit.TOPAZ_ORE), OreGenConfig.topaz_size.get(), OreGenConfig.topaz_min_height.get(), OreGenConfig.topaz_max_height.get()),
-    QUARTZ(Lazy.of(BlockInit.QUARTZ_ORE), OreGenConfig.quartz_size.get(), OreGenConfig.quartz_min_height.get(), OreGenConfig.quartz_max_height.get());
+    RUBY(Lazy.of(BlockInit.RUBY_ORE), Lazy.of(BlockInit.DEEPSLATE_RUBY_ORE), OreGenConfig.ruby_min_vein_size.get(), OreGenConfig.ruby_max_vein_size.get(), OreGenConfig.ruby_min_height.get(), OreGenConfig.ruby_max_height.get(), OreGenConfig.generate_ruby.get()),
+    SAPPHIRE(Lazy.of(BlockInit.SAPPHIRE_ORE), Lazy.of(BlockInit.DEEPSLATE_SAPPHIRE_ORE), OreGenConfig.sapphire_min_vein_size.get(), OreGenConfig.sapphire_max_vein_size.get(), OreGenConfig.sapphire_min_height.get(), OreGenConfig.sapphire_max_height.get(), OreGenConfig.generate_sapphire.get()),
+    TOPAZ(Lazy.of(BlockInit.TOPAZ_ORE), Lazy.of(BlockInit.DEEPSLATE_TOPAZ_ORE), OreGenConfig.topaz_min_vein_size.get(), OreGenConfig.topaz_max_vein_size.get(), OreGenConfig.topaz_min_height.get(), OreGenConfig.topaz_max_height.get(), OreGenConfig.generate_topaz.get()),
+    AMETHYST(Lazy.of(BlockInit.AMETHYST_ORE), Lazy.of(BlockInit.DEEPSLATE_AMETHYST_ORE), OreGenConfig.amethyst_min_vein_size.get(), OreGenConfig.amethyst_max_vein_size.get(), OreGenConfig.amethyst_min_height.get(), OreGenConfig.amethyst_max_height.get(), OreGenConfig.generate_amethyst.get()),
+    QUARTZ(Lazy.of(BlockInit.QUARTZ_ORE), Lazy.of(BlockInit.DEEPSLATE_QUARTZ_ORE), OreGenConfig.quartz_min_vein_size.get(), OreGenConfig.quartz_max_vein_size.get(), OreGenConfig.quartz_min_height.get(), OreGenConfig.quartz_max_height.get(), OreGenConfig.generate_quartz.get());
 
     private final Lazy<Block> block;
+    private final Lazy<Block> deepslateBlock;
+    private final int minVeinSize;
     private final int maxVeinSize;
     private final int minHeight;
     private final int maxHeight;
+    private final boolean generateOreToggle;
 
-    OreType(Lazy<Block> block, int maxVeinSize, int minHeight, int maxHeight) {
+    OreType(Lazy<Block> block, Lazy<Block> deepslateBlock, int minVeinSize, int maxVeinSize, int minHeight, int maxHeight, boolean generateOreToggle) {
         this.block = block;
+        this.deepslateBlock = deepslateBlock;
+        this.minVeinSize = minVeinSize;
         this.maxVeinSize = maxVeinSize;
         this.minHeight = minHeight;
         this.maxHeight = maxHeight;
+        this.generateOreToggle = generateOreToggle;
     }
 
     public Lazy<Block> getBlock() {
         return block;
+    }
+
+    public Lazy<Block> getDeepslateBlock() {
+        return deepslateBlock;
+    }
+
+    public int getMinVeinSize() {
+        return minVeinSize;
     }
 
     public int getMaxVeinSize() {
@@ -37,6 +52,10 @@ public enum OreType {
 
     public int getMaxHeight() {
         return maxHeight;
+    }
+
+    public boolean getGenerateOreToggle() {
+        return generateOreToggle;
     }
 
     public static OreType get(Block block) {
